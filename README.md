bfd6612d531fd38de9cec338d6e3f127cc23b30b
Robert Leibl <robert.leibl@haufe-lexware.com>
2019-12-11 13:45:14 +0100

    Create summary and print it at the end

--- README.md
+++ README.md
@@ -8,13 +8,28 @@ Words with different length are ignored.
 
 Example
 ```
-hotzenplotz <-> biometrical : 10
-hotzenplotz <-> biophysical : 11
-hotzenplotz <-> biopyribole : 10
-hotzenplotz <-> bioreaction : 10
+...
 hotzenplotz <-> biostatical : 11
 hotzenplotz <-> biotechnics : 10
 hotzenplotz <-> bipaleolate : 9
 hotzenplotz <-> biparasitic : 11
 hotzenplotz <-> bipartitely : 11
+...
+```
+
+Prints a Summary at the end
+```
+Summary:
+    Strings of different length:209873
+    Strings of Hamming distance 0:0
+    Strings of Hamming distance 1:0
+    Strings of Hamming distance 2:0
+    Strings of Hamming distance 3:0
+    Strings of Hamming distance 4:0
+    Strings of Hamming distance 5:0
+    Strings of Hamming distance 6:15
+    Strings of Hamming distance 7:80
+    Strings of Hamming distance 8:653
+    Strings of Hamming distance 9:2975
+    Strings of Hamming distance 10:9472
 ```
--- hotzenplotz.c
+++ hotzenplotz.c
@@ -10,13 +10,13 @@ int hamming_distance(unsigned x, unsigned y)
     return __builtin_popcount(x ^ y);
 }
 
-void hamming_distance_word(const char* a, const char* b) {
+int hamming_distance_word(const char* a, const char* b) {
 
 	int l = strlen(a);
 
 	if( l != strlen(b) ) {
 		// strings must be equal in length 
-		return;
+		return -1;
 	}
 
 	int distance = 0;
@@ -25,7 +25,7 @@ void hamming_distance_word(const char* a, const char* b) {
 			distance++;
 		}
 	}
-	printf("%s <-> %s : %d\n", a, b, distance);
+	return distance;
 }
 
 int main() {
@@ -38,17 +38,37 @@ int main() {
 		return -1;
 	}
 
-	//char line[256];
+	int* summary = malloc(strlen(hotzenplotz) * sizeof(int));
+	int no_length_match = 0;
+	for(int i=0; i<strlen(hotzenplotz); i++) {
+		summary[i] = 0;
+	}
+
 	char* word = NULL;
 	size_t len = 0;
 	ssize_t read;
+	int dist;
 	while ((read = getline(&word, &len, fp)) != -1) {
 
 		// getline returns the trailing '\n'
 		// remove it
 		word[ strlen(word)-1 ] = '\0';
 
-		hamming_distance_word(hotzenplotz, word);
+		dist = hamming_distance_word(hotzenplotz, word);
+		if( dist == -1 ) {
+			no_length_match++;
+		} else {
+
+			summary[dist] ++;
+
+			printf("%s <-> %s : %d\n", hotzenplotz, word, dist);
+		}
+	}
+
+	printf("Summary:\n");
+	printf("    Strings of different length:%d\n", no_length_match);
+	for(int i=0; i<strlen(hotzenplotz); i++) {
+		printf("    Strings of Hamming distance %d:%d\n", i, summary[i]);
 	}
 
 	fclose(fp);
